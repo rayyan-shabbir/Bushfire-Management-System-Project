92285b9f250d5c850c9b565bc95ac265b3520a8f
Rayyan Shabbir <rayanshabir1@gmail.com>
2024-05-05 15:37:19 +0500

    Add files via upload

---
+++ Bushfire JAVA/database/Dump20240429 (1)/bushfire.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (x86_64)
+--
+-- Host: localhost    Database: bushfire
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `drones`
+--
+
+DROP TABLE IF EXISTS `drones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `drones` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dname` varchar(50) DEFAULT NULL,
+  `dcoordinates` varchar(50) DEFAULT NULL,
+  `dmodel` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drones`
+--
+
+LOCK TABLES `drones` WRITE;
+/*!40000 ALTER TABLE `drones` DISABLE KEYS */;
+INSERT INTO `drones` VALUES (1,'Beta','-28.250,150.050','daz098'),(2,'Alpha','-24.31314,151.93605','daz123');
+/*!40000 ALTER TABLE `drones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reports` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `rname` varchar(50) DEFAULT NULL,
+  `rcoordinates` varchar(50) DEFAULT NULL,
+  `rdroneid` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rdronename_idx` (`rdroneid`),
+  CONSTRAINT `rdroneid` FOREIGN KEY (`rdroneid`) REFERENCES `drones` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reports`
+--
+
+LOCK TABLES `reports` WRITE;
+/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
+INSERT INTO `reports` VALUES (1,'daintree','-16.171689,145.4165567',2);
+/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trucks`
+--
+
+DROP TABLE IF EXISTS `trucks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trucks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tname` varchar(50) DEFAULT NULL,
+  `tcoordinates` varchar(50) DEFAULT NULL,
+  `tmodel` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trucks`
+--
+
+LOCK TABLES `trucks` WRITE;
+/*!40000 ALTER TABLE `trucks` DISABLE KEYS */;
+INSERT INTO `trucks` VALUES (1,'el-grande','-16.824303,145.6492395','trf125'),(2,'rambboo','-37.7197566,145.7478','trf759');
+/*!40000 ALTER TABLE `trucks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `uname` varchar(50) DEFAULT NULL,
+  `upwd` varchar(50) DEFAULT NULL,
+  `uemail` varchar(50) DEFAULT NULL,
+  `umobile` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','12345','admin1@gmail.com','0192873645'),(4,'rayyan','123','rayanshabir1@gmail.com','0918817277373');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-29 17:18:39
